55e7177e06cdb7b58bce43ab938e7a365bcfcccc
bchornii <bogdan.chorniy@gmail.com>
2017-08-13 23:29:12 +0300

    initial commit

---
+++ actions/actions.js
@@ -0,0 +1,32 @@
+/*
+	action types
+*/
+export const ActionTypes = {
+	ADD_TODO: 'ADD_TODO',
+	TOGGLE_TODO: 'TOGGLE_TODO',
+	SET_VISIBILITY_FILTER: 'SET_VISIBILITY_FILTER'
+};
+
+/*
+	filter types
+*/
+export const VisibilityFilters = {
+	SHOW_ALL: 'SHOW_ALL',
+  	SHOW_COMPLETED: 'SHOW_COMPLETED',
+  	SHOW_ACTIVE: 'SHOW_ACTIVE'
+};
+
+/*
+	action creators 
+*/
+export function addTodo(text) {
+	return {type: ActionTypes.ADD_TODO, text};
+}
+
+export function toggleTodo(index) {
+	return {type: ActionTypes.TOGGLE_TODO, index};
+}
+
+export function setVisibilityFilter(filter) {
+	return {type: ActionTypes.SET_VISIBILITY_FILTER, filter};
+}

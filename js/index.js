3f85b268ef1e8d181543d865dcee2ddca0ab399c
bchornii <bogdan.chorniy@gmail.com>
2017-09-12 23:41:16 +0300

    Integrated react router

--- js/index.js
+++ js/index.js
@@ -3,13 +3,8 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import todoApp from '../reducers/index'
-import App from '../components/App'
+import Root from '../components/Root'
 
 let store = createStore(todoApp)
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-)
+render(<Root />, document.getElementById('root'))
